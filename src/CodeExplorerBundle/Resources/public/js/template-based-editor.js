c36512455f3d4c082087efa5e097221cfeef7418
testp1 <franck_delahaye@yahoo.fr>
2016-04-28 18:36:34 +0200

    - test of assetic : commented as it doesn't work : details in french :   - Assetic est bugué pour le moment : après 1 journée de tentatives diverses, les fichier "ouput" des blocs twig "javascipts" et "stylesheet" tel que décrit dans les best practices ne sont pas dumper tel que décrit dans ces best practices, malgrè les nettoyage de cache et autre disable/enable du controller assetic tel que préconnisé dans les post sur google, bcp de post de signalement de ce bug sans réponse de solution qui a marché....  - Code doc editor (nickname "codoc") :    - very early prototyping of an editing context menu that can be triggered by a specific keyboard event ("Ctrl <anychar>" in this proto version...). The context menu is displayed where the mouse is to minimise mouse use. The menu items can be be navigated with keyboard arrows and enter to trigger the item action, sothat this menu can be totaly used either by the mouse or by the mouse or the keyboard, enabling fast "au fil de l'eau" authoring of the document content and structure whithout mouse interruption (this encourage to write more doc for the same time, which is a real issue for software documenting ;).     - the jquery plugin contextMenu plugin (https://swisnl.github.io/jQuery-contextMenu) has been integrated for that.   - Symfony demo fix : the cache of classe in app_dev.php has been commented, as it was not consistent with the code examples to disable class caching given in the best practices documentation.

--- src/CodeExplorerBundle/Resources/public/js/template-based-editor.js
+++ src/CodeExplorerBundle/Resources/public/js/template-based-editor.js
@@ -12,24 +12,72 @@
     init = tplbEditor.fn.init = function() {
         return this;
     };
+    tplbEditor.onClick = function (evt) {
+        tplbEditor.pageX=evt.pageX;
+        tplbEditor.pageY=evt.pageY;
+    };
+    tplbEditor.onKeyPress = function (evt) {
+            if (evt.defaultPrevented) {
+                return; // Should do nothing if the key event was already consumed.
+            }
+
+            if (evt.ctrlKey) {
+                //var theSel = document.getSelection()
+                    //var theNode = theSel.focusNode;
+                //var thePos = document.getSelection().
+                //var x = this.getSelection().
+                var $this = $(this);
+                $this.contextMenu();
+                //var evtCtx = $.Event("contextmenu", { data: evt.data, pageX: tplbEditor.pageX, pageY: tplbEditor.pageY });
+                //$this.trigger(evtCtx);
+                //jQuery(document.activeElement).triggerHandler('contextmenu',{pageX:tplbEditor.clientX,pageY:tplbEditor.clientY});
+                //.contextMenu(this.getCursorPosition());
+                    //.parent().parent().triggerHandler('contextmenu');
+                    //.parent().parent().contextMenu(this.getCursorPosition());
+                //parent().triggerHandler('contextmenu');
+
+/*
+// cf cette page https://developer.mozilla.org/fr/docs/Web/API/KeyboardEvent
+*/
+                //var codeOfCharE = 'e'.charCodeAt(0);
+                switch (evt.key) {
+                        case 'e':
+                            jQuery(getSelection().focusNode).contextMenu();
+                            break;
+                        default:
+                            return; // Quit when this doesn't handle the key event.
+                    }
+                    //return; // Quit when this doesn't handle the key event.
+            }
+
+            // Consume the event for suppressing "double action".
+            //evt.preventDefault();
+            return;
+//        alert('tplbEditor.onKeyPress');
+ //       alert(evt.target.localName);
+  //      alert(getSelection().focusNode.nodeName);
+    };
     tplbEditor.addTemplate = function (templateId) {
         var evt = ctxMenuEvt;
         var currentNode = $(evt.target);
         if (tplbEditor.selectedNode) currentNode = tplbEditor.selectedNode;
         var template = document.querySelector('#' + templateId);
         var clone = template.content.cloneNode(true);
-        //$('div',clone).attr('style','border-style:solid');
-        $('div',clone).attr('contextmenu','template-editor-contextmenu');
+        //$('div',clone).attr('style','border-style:dotted');
+        var theDiv=$('div',clone);
+        theDiv.attr('contextmenu','template-editor-contextmenu');
+        //theDiv.keypress(tplbEditor.onKeyPress);
+        //theDiv.children().attr('onkeypress','alert("ici")');
         currentNode.append(clone);
         if (tplbEditor.selectedNode) {
-            $(tplbEditor.selectedNode).css('background-color','');
+            $(tplbEditor.selectedNode).css('border-style','');
             tplbEditor.selectedNode=null;
         }
     };
     tplbEditor.selectParent = function () {
         var evt = ctxMenuEvt;
         var par = $(evt.target).parent();
-        par.css('background-color','yellow');
+        par.css('border-style','dotted');
         tplbEditor.selectedNode=par;
     };
     init.prototype = tplbEditor.fn;
@@ -42,8 +90,42 @@ addEventListener('contextmenu',function(evt){
 });
 jQuery( document ).ready(function() {
     try {
-        
-    } catch (e) {
-        alert ('error');
-    }
-});
+        jQuery('body').keypress(
+            tplbEditor.onKeyPress
+        ).click(
+            tplbEditor.onClick
+        );
+
+        /////////////////////
+
+
+                jQuery.contextMenu({
+                    selector: 'body',
+                    callback: function(key, options) {
+                        var m = "clicked: " + key + "on a " + this.localName;
+                        window.console && console.log(m) || alert(m);
+                    },
+/*                    position: function($menu,x,y){
+                        var dbgx= x;
+                        var dbgy=y;
+                    },*/
+                    determinePosition: function($menu){
+                        $menu.offset({top : tplbEditor.pageY, left : tplbEditor.pageX});
+                    },
+                    items: {
+                        "edit": {name: "Edit", icon: "edit"},
+                        "cut": {name: "Cut", icon: "cut"},
+                        copy: {name: "Copy", icon: "copy"},
+                        "paste": {name: "Paste", icon: "paste"},
+                        "delete": {name: "Delete", icon: "delete"},
+                        "sep1": "---------",
+                        "quit": {name: "Quit", icon: function(){
+                            return 'context-menu-icon context-menu-icon-quit';
+                        }}
+                    }
+                });
+            ////////////////////////
+        } catch (e) {
+            alert ('error');
+        }
+    });
